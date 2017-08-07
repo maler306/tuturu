4e352df20cf2ad5cd381eb8692492854e52b0deb
Yerkin <maler306@gmail.com>
2017-08-07 23:47:13 +0600

    Refactoring Carriage and train/show.html.erb

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,14 +4,12 @@ class Train < ApplicationRecord
   has_many :tickets
   has_many :carriages, dependent: :destroy
 
+  def sum_seats_by_type(carriage, type_seat)
+    carriages.where(type: carriage).sum(type_seat)
+  end
 
-
-    def sum_seats_by_type(carriage, type_seat)
-      carriages.where(type: carriage).sum(type_seat)
-    end
-
-    def sort_carriages
-      sort_flag ? carriages : carriages.reverse
-    end
+  def sort_carriages
+    sort_flag ? carriages : carriages.reverse
+  end
 
 end
