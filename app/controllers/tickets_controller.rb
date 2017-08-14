33bd0b41c543f6b224966b29e405eda726b397af
Yerkin <maler306@gmail.com>
2017-08-14 21:11:55 +0600

    Corrected indents

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,5 @@
 class TicketsController < ApplicationController
-before_action :set_ticket, only: [:show, :destroy]
+  before_action :set_ticket, only: [:show, :destroy]
 
   def index
   end
@@ -9,12 +9,12 @@ before_action :set_ticket, only: [:show, :destroy]
   end
 
   def create
-      @ticket = Ticket.new(ticket_params)
-      if @ticket.save
-        redirect_to @ticket
-      else
-        render :new
-      end
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render :new
+    end
   end
 
   def show
@@ -24,11 +24,11 @@ before_action :set_ticket, only: [:show, :destroy]
   end
 
   private
-    def set_ticket
-      @ticket = Ticket.find(params[:id])
-    end
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
 
-    def ticket_params
-      params.require(:ticket).permit(:user_id, :train_id, :departure_station_id, :arrival_station_id, :name, :surname, :doc_type, :doc_number, :doc_expire)
-    end
+  def ticket_params
+    params.require(:ticket).permit(:user_id, :train_id, :departure_station_id, :arrival_station_id, :name, :surname, :doc_type, :doc_number, :doc_expire)
+  end
 end
