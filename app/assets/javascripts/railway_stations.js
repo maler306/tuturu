9164188261ef79e5ee16fb2b9cb41c78cf976462
Yerkin <maler306@gmail.com>
2017-09-17 20:56:19 +0600

    search form inline editing

---
+++ app/assets/javascripts/railway_stations.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('a.edit_station').click(function(event) {
+    event.preventDefault();
+    var station_id;
+    var form;
+    var title;
+
+    station_id = $(this).data('stationId');
+    form = $('#edit_railway_station_' + station_id);
+    title = $('#railway_station_title_' + station_id);
+
+    if (!$(this).hasClass('Cancel')) {
+      $(this).html('Отменить');
+      $(this).addClass('Cancel');
+    } else {
+      $(this).html('Редактировать');
+      $(this).removeClass('Cancel');
+    }
+
+    form.toggle();
+    title.togle();
+
+  });
+});
